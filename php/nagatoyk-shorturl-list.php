eb8a563f7fe3a5fcbcb1ed967bea20f79c465c16
镜花水月 <393117228@qq.com>
2013-11-13 19:59:34 +0800

    Update nagatoyk-shorturl-list.php

--- php/nagatoyk-shorturl-list.php
+++ php/nagatoyk-shorturl-list.php
@@ -1,8 +1,8 @@
 <?php
 error_reporting(E_ALL^E_NOTICE);
 header('Content-type: text/html;charset=utf-8');
-$file 			= 'urls.txt';
-$use_rewrite 	= 1;
+$file = 'urls.txt';
+$use_rewrite = 1;
 if(!file_exists($file)){
 	fopen($file, 'w+');
 }
@@ -15,5 +15,5 @@ for($i = 0; $i < count($urls); $i++){
 	$filename = '/'.$filename[(count($filename) - 1)];
 	$shorturl = ($use_rewrite == 1) ? "http://{$_SERVER['HTTP_HOST']}{$dir}s/{$id}" : "http://{$_SERVER['HTTP_HOST']}{$dir}{$filename}?id={$id}";
 	$url = urldecode($urls[$i]);
-	echo $output = "<p>".sprintf("%04d", base_convert($id, '32', '10'))."{$l_yoururl} <a href=\"{$shorturl}\" onclick=\"window.open(this.href);return false\">{$shorturl}</a> --> {$url}</p>";
+	echo $output = "<p>".sprintf("%04d", base_convert($id, '32', '10'))."{$l_yoururl} <a href=\"{$shorturl}\" onclick=\"window.open(this.href);return false\">{$shorturl}</a></p>";
 }
